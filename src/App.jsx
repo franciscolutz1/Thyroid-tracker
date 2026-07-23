359adcd0b953ea7f4052f244be93d920ecaecf6f
franciscolutz1 <franciscolutz@gmail.com>
2026-07-23 14:16:40 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -627,6 +627,7 @@ const FOOD_DB = [
 { keys:["french fries", "fries"], name:"French Fries (Small, Deep-Fried)", cal:230, pro:2.5, carb:29, fat:11, fib:2.5, se:1, io:1, zn:0.4, ir:0.6, mg:22, vd:0 },
 { keys:["strawberry liquid yogurt", "liquid yogurt strawberry", "drinkable yogurt strawberry"], name:"Strawberry Liquid Yogurt (Small Bottle)", cal:110, pro:3, carb:20, fat:2, fib:0, se:5, io:20, zn:0.5, ir:0.1, mg:12, vd:0 },
   { keys:["red salami", "salami"], name:"Red Salami (4-5 slices)", cal:180, pro:9, carb:1, fat:15, fib:0, se:8, io:2, zn:1.2, ir:0.6, mg:8, vd:0.2 },
+ { keys:["baked chicken sweet potato broth", "chicken sweet potato onion broth"], name:"Baked Chicken Breast w/ Vegetable Broth, Sweet Potato & Onion", cal:420, pro:48, carb:35, fat:8, fib:5, se:38, io:8, zn:1.8, ir:2, mg:60, vd:0.2 },
   { keys:["chicken milanesa","milanesa de pollo"], name:"Chicken Milanesa", cal:350, pro:30, carb:20, fat:15, fib:1, se:24, io:2, zn:1.3, ir:1.2, mg:28, vd:0 },
  { keys:["pineapple peach oat chia smoothie", "smoothie pineapple peach banana"], name:"Pineapple Peach Banana Smoothie (w/ Oats, Chia, Greek Yogurt & Milk)", cal:475, pro:18, carb:76, fat:14, fib:11, se:8, io:45, zn:2, ir:1.5, mg:100, vd:2.5 },
   { keys:["havana alfajor","alfajor"], name:"Havana Alfajor", cal:200, pro:3, carb:30, fat:7, fib:1, se:1, io:2, zn:0.3, ir:0.5, mg:10, vd:0 },
